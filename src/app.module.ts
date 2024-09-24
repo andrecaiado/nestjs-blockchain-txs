d351fc5b176019386a76b47802ec3a05c0da8210
andrecaiado <andrecaiado@gmail.com>
2024-09-24 16:48:24 +0100

    Remove unused BlockchainService provider from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { WalletsModule } from './wallets/wallets.module';
 import { BlockchainModule } from './blockchain/blockchain.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { BlockchainService } from './blockchain/blockchain.service';
 
 @Module({
   imports: [WalletsModule, BlockchainModule, TransactionsModule],
-  providers: [BlockchainService],
 })
 export class AppModule {}
