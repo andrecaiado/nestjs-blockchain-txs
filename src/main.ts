bc2bcd2cafeb8dc69e79e5f089943b112d0a800f
andrecaiado <andrecaiado@gmail.com>
2024-10-06 18:09:19 +0100

    add swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Blockchain and Cryptocurrency transactions API')
+    .setDescription('Blockchain and Cryptocurrency transactions API')
+    .setVersion('1.0')
+    .setExternalDoc('Swagger JSON file', 'http://localhost:3000/api-json')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
