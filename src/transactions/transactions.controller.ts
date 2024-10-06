7d366d41bdc66165782abd2cb4f4e43514c0d031
andrecaiado <andrecaiado@gmail.com>
2024-10-06 18:08:11 +0100

    add swagger decorators to controllers

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Inject, Post } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionDto } from './dto/transaction.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Transactions')
 @Controller('transactions')
 export class TransactionsController {
   constructor(
@@ -9,6 +11,21 @@ export class TransactionsController {
     private readonly transactionsService: TransactionsService,
   ) {}
 
+  @ApiOperation({
+    summary: 'Submit transaction',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The transaction was submitted successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: `Transaction signature is invalid.\t\nThere are UTXOs in the inputs that do not belong to the sender.`,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: `There are UTXOs that are not unspent.\t\nInputs are not enough to cover the outputs.`,
+  })
   @Post()
   submitTransaction(@Body() transactionDto: TransactionDto): string {
     return this.transactionsService.submitTransaction(transactionDto);
--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,35 +1,99 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Transaction } from 'src/transactions/transaction';
 import { WalletDto } from './dto/wallet.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Wallet } from './wallet';
+import { TransactionDto } from 'src/transactions/dto/transaction.dto';
 
+@ApiTags('Wallets')
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
 
+  @ApiOperation({
+    summary: 'Create wallet',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The wallet was successfully created.',
+    type: WalletDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Wallet with the supplied name already exists.',
+  })
+  @HttpCode(HttpStatus.OK)
   @Post()
-  createWallet(@Body() createWalletDto: CreateWalletDto) {
+  createWallet(@Body() createWalletDto: CreateWalletDto): WalletDto {
     return this.walletsService.createWallet(createWalletDto);
   }
 
+  @ApiOperation({
+    summary: 'Get wallets',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'The wallets were successfully retrieved or returns an empty list.',
+    type: [WalletDto],
+  })
+  @HttpCode(HttpStatus.OK)
   @Get()
-  getWallets() {
+  getWallets(): WalletDto[] {
     return this.walletsService.getWallets();
   }
 
+  @ApiOperation({
+    summary: 'Get wallet by public key',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The wallet was successfully retrieved.',
+    type: WalletDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: `Wallet with the supplied public key was not found.`,
+  })
+  @HttpCode(HttpStatus.OK)
   @Get(':publicKey')
   getWallet(@Param('publicKey') publicKey: string): WalletDto {
     return this.walletsService.getWallet(publicKey);
   }
 
+  @ApiOperation({
+    summary: 'New transaction',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The new transaction was created successfully.',
+    type: TransactionDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: `Sender wallet with the supplied public key was not found.\t\nRecipient wallet with the supplied public key was not found.`,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: `Sender and recipient wallets cannot be the same.\t\nThe sender wallet has insufficient balance to fund the transaction.`,
+  })
+  @HttpCode(HttpStatus.OK)
   @Post(':publicKey/transactions')
-  @HttpCode(200)
   createTransaction(
     @Param('publicKey') publicKey: string,
     @Body() createTransactionDto: CreateTransactionDto,
-  ): Transaction {
+  ): TransactionDto {
     return this.walletsService.createTransaction(
       publicKey,
       createTransactionDto,
